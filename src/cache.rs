94e209abeafa80aeb010f81dd30c112eb250acac
David Hatch <dhatch387@gmail.com>
2019-05-29 21:45:39 -0400

    Add comments.

--- src/cache.rs
+++ src/cache.rs
@@ -31,6 +31,36 @@ impl <K, V> fmt::Debug for CacheValue<K, V> {
 intrusive_adapter!(CacheValueAdapter<K, V> = Rc<CacheValue<K, V>>: CacheValue<K, V> { link: LinkedListLink });
 
 
+/// LRUCache implements an in-memory cache of fixed capacity with a least-recency-used replacement
+/// policy.
+///
+/// The cache accepts any hashable and clonable value as a key type.
+///
+/// # Implementation Notes:
+///
+/// The LRUCache maintains a HashMap and doubly-linked-list to perform usage tracking.
+///
+/// Within both are reference-counted pointers to a CacheValue which implements an intrusive
+/// linked list. The instrusive list is necessary so that the LRU position can be updated in O(1)
+/// time (the linked list node is returned by the map.
+///
+/// The reference-counted pointers are required because Rust does not support self-referential
+/// structs. (took me some time to realize this).
+///
+/// # Concurrency:
+///
+/// ...
+///
+/// # Alternative implementations:
+///
+/// ...
+///
+/// # Concerns:
+///
+/// This data structure is pretty poor for cache-locality (if I am understanding Rc correctly).
+/// Each value is separately allocated, so the data the cache points to will not be brought into
+/// cache together.  Ideally, we would allocate the memory that each Rc points to from a single
+/// buffer.
 pub struct LRUCache<K: Eq + std::hash::Hash + Clone, V> {
     map: HashMap<K, Rc<CacheValue<K, V>>>,
     lru_list: RefCell<LinkedList<CacheValueAdapter<K, V>>>,
@@ -39,7 +69,15 @@ pub struct LRUCache<K: Eq + std::hash::Hash + Clone, V> {
 
 
 impl <K: Eq + std::hash::Hash + Clone, V> LRUCache<K, V> {
-    /// Create a LRUCache with space for ``capacity`` items.
+    /// Create a LRUCache with space for `capacity` items.
+    ///
+    /// # Arguments:
+    ///
+    /// - `capacity`: The maximum number of items permitted in the cache.
+    ///
+    /// # NB:
+    ///
+    /// - The cache will allocate memory for all items, even if it is not full.
     pub fn new(capacity: usize) -> LRUCache<K, V> {
         LRUCache {
             map: HashMap::with_capacity(capacity),
@@ -48,7 +86,7 @@ impl <K: Eq + std::hash::Hash + Clone, V> LRUCache<K, V> {
         }
     }
 
-    /// Return the value in the cache for ``key``, if it exists.
+    /// Get the value for `key` in `self`, if it exists.  Otherwise, return `None`.
     pub fn get(&self, key: &K) -> Option<&V> {
         match self.map.get(key) {
             None => None,
@@ -59,10 +97,15 @@ impl <K: Eq + std::hash::Hash + Clone, V> LRUCache<K, V> {
         }
     }
 
-    /// Put ``value`` into cache for ``key``.
-    /// Returns the previous value if there was one.
+    /// Put `value` into `self` for `key`.
+    ///
+    /// # Returns
+    ///
+    /// The previous value in the cache, or `None`.
     pub fn put(&mut self, key: K, value: V) -> Option<V> {
         let cache_value = Rc::new(CacheValue::new(key.clone(), value));
+
+        // We only need to make room for a new value if we are not replacing an old one.
         if !self.map.contains_key(&key) {
             self.make_room();
         }
@@ -72,10 +115,17 @@ impl <K: Eq + std::hash::Hash + Clone, V> LRUCache<K, V> {
             Some(cache_value) => {
                 let value;
 
+                // This unsafe block is required to remove the item from the intrusive linked list
+                // contained in `CacheValue`.
+                //
+                // Assumes that cache_value is already in `lru_list`.
                 unsafe {
                     let raw = Rc::into_raw(cache_value);
                     let mut cursor = self.lru_list.get_mut().cursor_mut_from_ptr(raw);
                     value = cursor.remove();
+
+                    // Converts raw pointer back into a `Rc<CacheValue>` that can be dropped at the
+                    // end of this scope.
                     Rc::from_raw(raw);
                 }
 
@@ -98,9 +148,12 @@ impl <K: Eq + std::hash::Hash + Clone, V> LRUCache<K, V> {
 
     /// Update access tracking, indicating that a cache value has been accessed.
     ///
-    /// Safety Note:
-    ///   - Assumes that ``cache_value`` is already in lru_list.  If not, behavior is
-    ///     undefined.
+    /// Moves `cache_value` to the front of `lru_list`, indicating it has been used most recently.
+    ///
+    /// # Safety
+    ///
+    /// - Assumes that ``cache_value`` is already in lru_list.  If not, behavior is
+    ///   undefined.
     fn touch(&self, cache_value: &CacheValue<K, V>) {
         let mut lru_list = self.lru_list.borrow_mut();
 
--- src/main.rs
+++
@@ -1,8 +0,0 @@
-extern crate intrusive_collections;
-
-mod cache;
-
-fn main() {
-    let cache: cache::LRUCache<u64,u64> = cache::LRUCache::new(10);
-    println!("Hello, world!");
-}
